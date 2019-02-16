b9492f5807c59b21cd11fdaf1976e6366b37fd37
kongaman <christian.kranz@gmail.com>
2019-02-16 11:46:42 +0100

    configured and prepared

--- src/main/java/com/bharath/restws/RestattachmentsApplication.java
+++ src/main/java/com/bharath/restws/RestattachmentsApplication.java
@@ -1,4 +1,4 @@
-package com.bharath.restwsclient;
+package com.bharath.restws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
