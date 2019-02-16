8a21d1347c0e3e851c65d7779c92c3777a486c30
kongaman <christian.kranz@gmail.com>
2019-02-16 12:57:13 +0100

    created endpoint

--- src/main/java/com/bharath/restws/FileService.java
+++ src/main/java/com/bharath/restws/FileService.java
@@ -6,10 +6,28 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+
+
+@Path("/fileservice") // has to be same path as in html-form <form action="services/fileService/upload...."
 public class FileService {
 
-	private static final String FILE_PATH = "";
+	private static final String FILE_PATH = "D:\\RestUploadTest";
+	
+	@Path("/upload") // has to be same path as in html-form <form action="services/fileService/upload...."
+	@POST
+	public void upload(List<Attachment> attachments) throws FileNotFoundException, IOException {
+		
+		for (Attachment attachment : attachments) {
+			copyFile(attachment.getDataHandler().getInputStream());
+		}
+		
+	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
